a7be200a0893baa6c75c39f6dae00e67d32209d5
Joseph Naberhaus <naberhausj@gmail.com>
2021-10-08 23:45:37 -0500

    feat: add example app and implement up/down movement

---
+++ example/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"github.com/JosephNaberhaus/go-text-editor"
+	"github.com/ahmetb/go-cursor"
+	"github.com/eiannone/keyboard"
+	"os"
+)
+
+func main() {
+	editor := go_text_editor.NewEditor()
+	editor.SetWidth(20)
+
+	err := keyboard.Open()
+	if err != nil {
+		fmt.Printf("Error starting keyboard listener")
+		os.Exit(2)
+	}
+	defer func () {
+		err := keyboard.Close()
+		if err != nil {
+			fmt.Printf("Error closing keyboard listener")
+		}
+	}()
+
+	for {
+		fmt.Printf(cursor.ClearEntireScreen())
+		fmt.Printf(cursor.MoveTo(0, 0))
+		fmt.Printf("Press ESC to exit...\n")
+		fmt.Printf(editor.String())
+		fmt.Printf(cursor.MoveTo(editor.CursorRow() + 2, editor.CursorColumn() + 1))
+
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			fmt.Printf("Error getting key")
+			break
+		}
+
+		if char != 0 {
+			editor.Write(string(char))
+		} else {
+			switch key {
+			case keyboard.KeyEsc: return
+			case keyboard.KeyArrowLeft: editor.Left()
+			case keyboard.KeyArrowRight: editor.Right()
+			case keyboard.KeyArrowUp: editor.Up()
+			case keyboard.KeyArrowDown: editor.Down()
+			case keyboard.KeyBackspace: fallthrough
+			case keyboard.KeyBackspace2: editor.Backspace()
+			case keyboard.KeyEnter: editor.Newline()
+			case keyboard.KeySpace: editor.Write(" ")
+			}
+		}
+	}
+}
