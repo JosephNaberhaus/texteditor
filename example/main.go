1b852557494970943fc2898e6535f911468156a0
Joseph Naberhaus <naberhausj@gmail.com>
2021-11-25 22:56:32 -0600

    feat: fix some incorrect cursor behavior

--- example/main.go
+++ example/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/JosephNaberhaus/go-text-editor"
+	editor "github.com/JosephNaberhaus/go-text-editor"
 	"github.com/ahmetb/go-cursor"
 	"github.com/eiannone/keyboard"
 	"os"
 )
 
 func main() {
-	editor := go_text_editor.NewEditor()
+	editor := editor.NewEditor()
 	editor.SetFirstLineIndent(4)
 	editor.SetWidth(20)
 
